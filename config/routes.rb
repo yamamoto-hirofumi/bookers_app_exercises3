256579a99092deb15498d41de8065b781d59a9f5
yamamoto-hirofumi <sun.lionking.88@gmail.com>
2021-07-27 02:37:22 +0000

    [start]

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  root to: 'homes#top'
+  resources :books
+  
+  get 'home/about' => 'homes#about'
+  resources :users,only: [:show,:index,:edit,:update]
+end
